37c2166cda94162389178539a85f198190396bf8
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-05-03 22:28:54 +0200

    creating my first action

---
+++ app/actions/dashboard-actions.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import prisma from '@/lib/prisma'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export const getImagesAction = async () => {
+  try {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    })
+    if (!session?.session || !session?.user) {
+      redirect('/login')
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: session.user.id,
+      },
+      include: {
+        images: true,
+      },
+    })
+
+    if (user?.images) {
+      return user.images || []
+    }
+  } catch (e) {
+    console.error('Please Log in first', e)
+  }
+}
--- app/dashboard/library/page.tsx
+++ app/dashboard/library/page.tsx
@@ -1,46 +1,64 @@
-import prisma from '@/lib/prisma'
-import { auth } from '@/lib/auth'
-import { headers } from 'next/headers'
+'use client'
+
+import { getImagesAction } from '@/app/actions/dashboard-actions'
 import { Copy } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
-const Library = async () => {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  })
-  if (!session?.session || !session?.user) {
-    return <div>Not authenticated</div>
-  }
+const Library = () => {
+  const [images, setImages] = useState([])
+  const [isLoading, setLoading] = useState(true)
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: session.user.id,
-    },
-    include: {
-      images: true,
-    },
-  })
+  useEffect(() => {
+    const fetchImages = async () => {
+      const fetchedImages = await getImagesAction()
+      setLoading(false)
+      setImages(fetchedImages)
+    }
+    fetchImages()
+  }, [])
 
-  console.log(user.images)
+  if (isLoading)
+    return (
+      <div className="h-screen flex flex-col items-center justify-center"></div>
+    )
 
   return (
     <div>
-      <h1>Welcome {session.user.name}</h1>
-      <p>Your images are here</p>
-      <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-4">
-        {user?.images &&
-          user.images.map((img) => (
+      <h1 className="text-center text-3xl mt-2">Your uploaded images</h1>
+      <p className="text-center text-xl my-8">
+        Please hover and copy any of the images, you are willing to share with
+        someone
+      </p>
+      <div className="grid md:grid-cols-3 xl:grid-cols-4 gap-4">
+        {images.length > 0 &&
+          images.map((img) => (
             <div
-              className="group w-full h-64 relative cursor-pointer border-4 border-black rounded-md overflow-hidden bg-black/80"
+              className="flex flex-col items-center justify-center"
               key={img.path}
             >
-              <img
-                src={`https://${process.env.AWS_DISTRIBUTION_URL}/${img.path}`}
-                alt="Zoom on hover"
-                className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105 opacity-100 transition-opacity duration-300 group-hover:opacity-10"
-              />
-              <p className="absolute text-center left-1/2 -translate-x-1/2 top-1/2 -translate-y-[50%] text-3xl text-white transition-opacity duration-300 ease-in-out opacity-0 group-hover:opacity-100">
-                <Copy className="size-24" />
-              </p>
+              <div className="group w-full h-44 relative cursor-pointer border-4 border-black rounded-md overflow-hidden bg-black/80">
+                <img
+                  src={`https://${process.env.NEXT_PUBLIC_AWS_DISTRIBUTION_URL}/${img.path}`}
+                  alt="Zoom on hover"
+                  className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105 opacity-100 transition-opacity duration-300 group-hover:opacity-10"
+                />
+                <p className="absolute text-center left-1/2 -translate-x-1/2 top-1/2 -translate-y-[50%] text-3xl text-white transition-opacity duration-300 ease-in-out opacity-0 group-hover:opacity-100">
+                  <Copy
+                    size={36}
+                    onClick={() => {
+                      navigator.clipboard.writeText(
+                        `https://${process.env.AWS_DISTRIBUTION_URL}/${img.path}`,
+                      )
+                    }}
+                  />
+                </p>
+              </div>
+              <span className="text-slate-700 font-bold">
+                Created at: {img.createdAt.toLocaleDateString()}
+              </span>
+              <span className="text-slate-700 font-bold text-ellipsis">
+                Name: {img.id.split('.')[0]}
+              </span>
             </div>
           ))}
       </div>
